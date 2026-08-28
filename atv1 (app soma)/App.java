8711337c89826b1c2ab3a989beba2220484a0c10
Arthur Apolucena <apolucenaarthursantos@gmail.com>
2026-08-28 14:26:56 -0300

    atv1: app soma

---
+++ atv1 (app soma)/App.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String strN1, strN2;
+        float fltN1, fltN2, fltResultado;
+
+        strN1 = JOptionPane.showInputDialog(null, "Digite o primeiro valor: ");
+        strN2 = JOptionPane.showInputDialog(null, "Digite o segundo valor: ");
+
+        fltN1 = Float.parseFloat(strN1);
+        fltN2 = Float.parseFloat(strN2);
+
+        fltResultado = fltN1 + fltN2;
+
+        JOptionPane.showMessageDialog(null, "O resultado da soma é: " + fltResultado);
+    }
+}
